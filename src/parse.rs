03099607285f6f1c5c073e0959c631b5524c3be8
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-02-21 21:38:59 -0700

    Fixed parse?

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,6 @@
 use std::{
     num::ParseIntError,
+    ops::RangeFrom,
     str::{self, FromStr},
 };
 
@@ -7,16 +8,16 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::{
-        complete::{char, digit1, line_ending, one_of},
+        complete::{self as cc, char, digit1, line_ending, one_of},
         streaming::space1,
     },
-    combinator::{all_consuming, map_res},
-    error::{FromExternalError, ParseError},
+    combinator::all_consuming,
+    error::ParseError,
     multi::separated_list1,
     sequence::{preceded, terminated, tuple},
-    AsChar, IResult, InputLength, InputTakeAtPosition, Parser,
+    AsChar, IResult, InputIter, InputLength, Slice,
 };
-use nom_locate::{position, LocatedSpan};
+use nom_locate::LocatedSpan;
 
 pub type Span<'a> = LocatedSpan<&'a str>;
 
@@ -50,29 +51,28 @@ pub struct Monkey {
     test_false_target: usize,
 }
 
-fn digit1_to_num<'a, O, E>(i: Span<'a>) -> IResult<Span<'a>, O, E>
+fn digit1_to_num<'a, I, O, E>(i: I) -> IResult<I, O, E>
 where
-    O: FromStr,
-    E: ParseError<Span<'a>>
-        + FromExternalError<Span<'a>, E>
-        + FromExternalError<Span<'a>, <O as FromStr>::Err>,
+    O: From<u8>,
+    I: InputIter + Slice<RangeFrom<usize>> + InputLength,
+    <I as InputIter>::Item: AsChar,
+    E: ParseError<I>,
 {
-    map_res(digit1, |s: Span<'a>| s.parse::<O>())(i)
+    // Extra trait required by below makes it very hard to use in tuple and preceded etc.
+    // map_res(digit1, |s: I| s.into().parse::<O>())(i)
+    cc::u8(i).map(|(input, out)| (input, out.into()))
 }
 
 fn starting_items<'a, E>(i: Span<'a>) -> IResult<Span<'a>, Vec<u8>, E>
 where
-    E: ParseError<Span<'a>> + ParseError<&'a str>,
+    E: ParseError<Span<'a>>,
 {
     let (i, starting_items) = preceded(
         tag("  Starting items: "),
-        separated_list1(
-            tag(", "),
-            digit1.map(|s| u8::from_str_radix(s, 10).unwrap()),
-        ),
-    )(*i)?;
+        separated_list1(tag(", "), digit1_to_num),
+    )(i)?;
     let (i, _) = line_ending(i)?;
-    Ok((i.into(), starting_items))
+    Ok((i, starting_items))
 }
 
 fn operation<'a, E>(i: Span<'a>) -> IResult<Span<'a>, Operation, E>
@@ -104,23 +104,15 @@ where
 
 fn parse_monkey<'a, T, E>(i: Span<'a>) -> IResult<Span<'a>, Monkey, E>
 where
-    T: FromStr<Err = ParseIntError>,
-    E: ParseError<Span<'a>>
-        + FromExternalError<Span<'a>, <T as FromStr>::Err>
-        + FromExternalError<Span<'a>, E>
-        + ParseError<&'a str>,
+    E: ParseError<Span<'a>>,
 {
     let (i, _) = tuple((tag("Monkey"), terminated(digit1, char(':'))))(i)?;
     let (i, _) = line_ending(i)?;
     let (i, starting_items) = starting_items(i)?;
     let (i, op) = operation(i)?;
-    let (i, test_divisor) = preceded(
-        tag("  Test: divisible by "),
-        digit1.map(|s| u8::from_str_radix(s, 10).unwrap()),
-    )(*i)?;
+    let (i, test_divisor) = preceded(tag("  Test: divisible by "), digit1_to_num)(i)?;
     let (i, _) = line_ending(i)?;
-    let (i, test_true_target) =
-        preceded(tag("    If true: throw to monkey "), digit1_to_num)(i.into())?;
+    let (i, test_true_target) = preceded(tag("    If true: throw to monkey "), digit1_to_num)(i)?;
     let (i, _) = line_ending(i)?;
     let (i, test_false_target) = preceded(tag("    If false: throw to monkey "), digit1_to_num)(i)?;
     let (i, _) = line_ending(i)?;
@@ -138,11 +130,7 @@ where
 
 pub fn parse_input<'a, T, E>(input: Span<'a>) -> IResult<Span<'a>, Vec<Monkey>, E>
 where
-    T: FromStr<Err = ParseIntError>,
-    E: ParseError<Span<'a>>
-        + FromExternalError<Span<'a>, <T as FromStr>::Err>
-        + FromExternalError<Span<'a>, E>
-        + ParseError<&'a str>,
+    E: ParseError<Span<'a>>,
 {
     all_consuming(separated_list1(line_ending, parse_monkey::<'a, T, E>))(input)
 }
