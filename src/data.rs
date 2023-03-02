0fa745131430d1a356e4737469f2d4ec7f42570a
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-03-01 20:16:55 -0700

    Finished

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,149 @@
+use num::NumCast;
+
 use crate::cord::Cord;
+use std::{
+    fmt::Debug,
+    ops::{Deref, RangeInclusive, Sub},
+};
 
 #[derive(Debug)]
 pub struct Pair<T> {
     pub sensor: Cord<T>,
     pub beacon: Cord<T>,
+}
+
+#[derive(PartialEq, Eq, Clone, Hash)]
+pub struct SortableRangeInclusive<T>(pub RangeInclusive<T>);
+
+impl<T: Debug> Debug for SortableRangeInclusive<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("S").field(&self.0).finish()
+    }
+}
+
+impl<T> Deref for SortableRangeInclusive<T> {
+    type Target = RangeInclusive<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> From<RangeInclusive<T>> for SortableRangeInclusive<T> {
+    fn from(value: RangeInclusive<T>) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> From<SortableRangeInclusive<T>> for RangeInclusive<T> {
+    fn from(value: SortableRangeInclusive<T>) -> Self {
+        value.0
+    }
+}
+
+impl<T: PartialOrd + Ord + Clone> PartialOrd for SortableRangeInclusive<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: PartialOrd + Ord + Clone> Ord for SortableRangeInclusive<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        let order = match self.start().cmp(other.start()) {
+            std::cmp::Ordering::Equal => match self.end().cmp(other.end()) {
+                std::cmp::Ordering::Equal => std::cmp::Ordering::Equal, // If both bounds equal then they are equal.
+                x => x, // If greater/less return that.
+            },
+            x => x, // If greater/less return that.
+        };
+
+        order
+    }
+}
+
+impl<T: PartialOrd + Ord + Clone + Sub<Output = T> + NumCast> SortableRangeInclusive<T> {
+    // Takes two ranges in sorted order. Returns the merged array and any remaining range if it exists.
+    pub fn merge(self, other: Self) -> (Self, Option<Self>) {
+        // Check for overlap
+        if self.end() >= &(other.start().clone() - num::cast(1).unwrap()) {
+            // If other ends after self meaning they meet in the middle.
+            if self.end() <= other.end() {
+                return (
+                    SortableRangeInclusive(self.start().clone()..=other.end().clone()),
+                    None,
+                );
+            }
+            // If self completely encloses other.
+            else {
+                return (self, None);
+            }
+        }
+        (self, Some(other))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SortableRangeInclusive;
+
+    #[test]
+    fn merge_test() {
+        assert_eq!(
+            SortableRangeInclusive(1..=2).merge(SortableRangeInclusive(2..=3)),
+            (SortableRangeInclusive(1..=3), None)
+        );
+        assert_eq!(
+            SortableRangeInclusive(1..=2).merge(SortableRangeInclusive(1..=4)),
+            (SortableRangeInclusive(1..=4), None)
+        );
+        assert_eq!(
+            SortableRangeInclusive(1..=2).merge((3..=4).into()),
+            ((1..=4).into(), None)
+        );
+        /* (-4..=8), S(9..=9) */
+        assert_eq!(
+            SortableRangeInclusive(-4..=8).merge((9..=11).into()),
+            ((-4..=11).into(), None)
+        )
+    }
+
+    #[test]
+    fn range_sort_order() {
+        dbg!((3..2).ge(2..3)); // first range is equivalent to empty so it should go first on lexicographic compare.
+        dbg!((3..2).le(2..3));
+        dbg!((3..=2).ge(2..=3));
+        dbg!((3..=2).le(2..=3));
+        let range_sort_key = |x: &std::ops::RangeInclusive<i32>,
+                              y: &std::ops::RangeInclusive<i32>| {
+            if x.clone().le(y.clone()) {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
+        };
+        let mut out = vec![1..=2, 2..=3, 1..=4];
+        out.sort_by(|x, y| range_sort_key(x, y));
+        dbg!(out);
+        let mut out = vec![1..=6, 3..=2, 6..=6, 5..=7, 5..=7, 2..=3, 1..=4];
+        out.sort_by(|x, y| range_sort_key(x, y));
+        dbg!(&out, out[0].clone().cmp(out[1].clone()));
+
+        let mut out = vec![
+            SortableRangeInclusive(1..=2),
+            SortableRangeInclusive(2..=3),
+            SortableRangeInclusive(1..=5),
+            SortableRangeInclusive(3..=4),
+        ];
+        out.sort();
+        assert_eq!(
+            out,
+            vec![
+                (1..=2).into(),
+                (1..=5).into(),
+                (2..=3).into(),
+                (3..=4).into()
+            ]
+        );
+        dbg!(out);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::cord::Cord;
+use crate::data::SortableRangeInclusive;
 #[allow(unused_imports)]
 use advent_15::dbc;
 use std::{error::Error, fs};
@@ -8,7 +9,7 @@ mod parse;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Part 1 answer: {:#?}", part1::run("input.txt", 2000000)?);
-    // println!("Part 2 answer: {:#?}", part2::run("input.txt", 4000000)?);
+    println!("Part 2 answer: {:#?}", part2::run("input.txt", 4000000)?);
     Ok(())
 }
 
@@ -45,10 +46,10 @@ mod part1 {
             }
         }
 
-        // Only care about safe spots on the same row
         Ok(safe_spots.len())
     }
 
+    // Takes too long
     #[allow(dead_code)]
     pub fn run_naive(file: &str, row: isize) -> Result<usize, Box<dyn Error>> {
         let input_str = fs::read_to_string(file)?;
@@ -68,16 +69,66 @@ mod part1 {
 
 mod part2 {
     use super::*;
-    use std::collections::HashSet;
+    use std::{collections::HashSet, vec};
     pub fn run(file: &str, upper_limit: isize) -> Result<usize, Box<dyn Error>> {
+        let input_str = fs::read_to_string(file)?;
+        let input = Box::leak(Box::new(input_str));
+        let (_, parsed_input) = parse::parse_input::<isize>(input)?;
+
+        let mut row_ranges = vec![vec![]; (upper_limit + 1).try_into().unwrap()];
+        // Add all sensors to the rows they affect
+        for pair in parsed_input {
+            let radius = pair.sensor.manhattan_distance(&pair.beacon);
+            for row in (pair.sensor.1 - radius)..=(pair.sensor.1 + radius) {
+                if 0 <= row && row <= upper_limit {
+                    let row_idx: usize = row.try_into().unwrap();
+                    // Project the leftover range onto the row.
+                    let leftover = radius
+                        .saturating_sub(isize::try_from(row.abs_diff(pair.sensor.1)).unwrap());
+                    row_ranges[row_idx].push(SortableRangeInclusive::from(
+                        (pair.sensor.0 - leftover)..=(pair.sensor.0 + leftover),
+                    ));
+                }
+            }
+        }
+        // Merge ranges for each range until a row is found that has two un-mergable ranges.
+        let mut distress_beacon_x: Option<usize> = None;
+        let mut distress_beacon_y: Option<usize> = None;
+        // Reverse because location is closer to end then beginning.
+        'rowloop: for (row_idx, row) in row_ranges.iter_mut().enumerate().rev() {
+            row.sort();
+            // Merge ranges while possible.
+            while row.len() >= 2 {
+                let merged_ranges = row[0].clone().merge(row[0 + 1].clone());
+                // If the second range couldn't be merged into the first found the un-numerable row. The start of the second range -1 is the beacon location.
+                if let Some(x) = merged_ranges.1 {
+                    distress_beacon_x = Some((x.start() - 1).try_into().unwrap());
+                    distress_beacon_y = Some(row_idx);
+                    break 'rowloop;
+                }
+                // Replace old ranges with new merged range.
+                row.remove(0); // Remove first element.
+                row[0] = merged_ranges.0; // Replace second element (now first because first element was removed shifting everything down) with merged replacement.
+            }
+        }
+        Ok(distress_beacon_x.unwrap() * 4000000 + distress_beacon_y.unwrap())
+    }
+
+    // Takes too long and allocates too much.
+    #[allow(dead_code, unused_variables)]
+    pub fn run_naive(file: &str, upper_limit: isize) -> Result<usize, Box<dyn Error>> {
         let input_str = fs::read_to_string(file)?;
         let input = Box::leak(Box::new(input_str));
         let possible_locations = Cord(0, 0)
             .interpolate(&Cord(upper_limit, upper_limit))
-            .into_iter()
-            .map(|x| dbc!(x))
+            .map(|x| {
+                if x.0 % 100000 == 0 && x.1 % 100000 == 0 {
+                    print!("{x:?}");
+                }
+                x
+            })
             .collect::<HashSet<_>>();
-        dbg!("possible.");
+        dbg!("possible.", possible_locations.len());
         todo!();
     }
 }
@@ -109,6 +160,12 @@ mod tests {
     #[test]
     fn test_part2() -> Result<(), Box<dyn Error>> {
         assert_eq!(part2::run("inputtest.txt", 20)?, 56000011);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2_ans() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2::run("input.txt", 4000000)?, 11558423398893);
         Ok(())
     }
 }
