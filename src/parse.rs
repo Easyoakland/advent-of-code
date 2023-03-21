f7ca05282a5a7d85d825155c96ff6ffcfbe2c775
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-03-21 13:08:35 -0600

    Added parse_from for nom parsers.

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,31 @@
 use std::fs;
 
+use nom::{error::ParseError, IResult, Parser};
+use std::str::FromStr;
+
 pub fn read_file_static(file_path: &str) -> Result<&'static str, std::io::Error> {
     let input_str = fs::read_to_string(file_path)?;
     Ok(Box::leak(Box::new(input_str)))
+}
+
+/// Takes a combinator and converts the output to a &str before parsing with `str.parse()`. Returns `nom::error::ErrorKind::Fail` on fail.
+pub fn parse_from<'a, F, I, O, E>(mut f: F) -> impl FnMut(I) -> IResult<I, O, E>
+where
+    F: Parser<I, I, E>,
+    O: FromStr,
+    I: Into<&'a str>,
+    E: ParseError<I>,
+{
+    move |input: I| {
+        let (input, out) = f.parse(input)?;
+        let Ok(out) = (out.into()).parse::<O>()
+        else {
+            // If there is FromStr error the output type can't be parsed from the original combinator's output.
+            return Err(nom::Err::Failure(E::from_error_kind(
+                input,
+                nom::error::ErrorKind::Fail,
+            )));
+        };
+        Ok((input, out))
+    }
 }
