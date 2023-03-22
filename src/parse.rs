491866329fd175c3e0a5d4270be5393a236be627
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-03-22 00:01:19 -0600

    Added signed parser

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,11 @@
-use std::fs;
-
-use nom::{error::ParseError, IResult, Parser};
-use std::str::FromStr;
+use nom::{
+    character::complete::one_of,
+    combinator::{opt, recognize},
+    error::ParseError,
+    sequence::tuple,
+    AsChar, FindToken, IResult, InputIter, Parser,
+};
+use std::{fs, str::FromStr};
 
 pub fn read_file_static(file_path: &str) -> Result<&'static str, std::io::Error> {
     let input_str = fs::read_to_string(file_path)?;
@@ -27,5 +31,56 @@ where
             )));
         };
         Ok((input, out))
+    }
+}
+
+/// Parses a `+` or `-` symbol preceding the parser that may or may not be preceded by a `-`.
+pub fn signed<'a, F, I, E>(f: F) -> impl FnMut(I) -> IResult<I, I, E> + 'a
+where
+    F: Parser<I, I, E> + 'a,
+    I: Into<&'a str>
+        + Clone
+        + nom::Slice<std::ops::RangeFrom<usize>>
+        + nom::InputIter
+        + nom::Offset
+        + nom::Slice<std::ops::RangeTo<usize>>
+        + 'a,
+    E: ParseError<I> + 'a,
+    <I as InputIter>::Item: AsChar + Copy,
+    &'a str: FindToken<<I as InputIter>::Item>,
+{
+    recognize(tuple((opt(one_of("-+")), f)))
+}
+
+mod test {
+    // For some reason the imports are indicated as unused but they aren't.
+    #[allow(unused_imports)]
+    use super::*;
+    #[allow(unused_imports)]
+    use nom::character::complete::digit1;
+    #[allow(unused_imports)]
+    use nom::error::Error;
+
+    #[test]
+    fn parse_from_test() {
+        let input = "1234";
+        assert_eq!(
+            parse_from::<_, _, u32, Error<_>>(digit1)(input),
+            Ok(("", 1234))
+        );
+    }
+
+    #[test]
+    fn parse_from_signed_test() {
+        let input = "-1234";
+        assert_eq!(
+            parse_from::<_, _, i32, Error<_>>(signed(digit1))(input),
+            Ok(("", -1234))
+        );
+        let input = "+1234";
+        assert_eq!(
+            parse_from::<_, _, u32, Error<_>>(signed(digit1))(input),
+            Ok(("", 1234))
+        );
     }
 }
