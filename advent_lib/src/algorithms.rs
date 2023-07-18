e950532e0271c891fae93adf63932a468e625969
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-07-17 23:45:44 -0600

    Backtrack for astar.

--- advent_lib/src/algorithms.rs
+++ advent_lib/src/algorithms.rs
@@ -18,24 +18,32 @@ pub fn astar<Node, Distance, I>(
     potential: impl Fn(Node) -> Distance,
     neighbor_edge_weight: impl Fn(Node, Node) -> Distance,
     reconstruct_path: bool,
-) -> Option<(Distance, Option<Vec<Node>>>)>
+) -> Option<(Distance, Option<Vec<Node>>)>
 where
     Node: Eq + Hash + Copy,
     Distance: Zero + Add<Output = Distance> + Ord + Copy,
     I: Iterator<Item = Node>,
 {
-    fn reconstruct_path(backtrack: HashMap<Node, Option<Node>>) -> Vec<Node> {
-        todo!()
-    }
     // Boundary nodes are the nodes at the edge to pick from to explore next.
     let mut boundary_nodes = HashSet::from([start]);
     // Distances is a map of the shortest known distance to any node from the start.
     let mut distances = HashMap::from([(start, Distance::zero())]);
     // Reconstructed path is the shortest path from the start to the end. Each node knows its parent and this means the path can be backtracked.
-    let backtrack = if backtrack {
+    let mut backtrack = if reconstruct_path {
         Some(HashMap::from([(start, None)]))
     } else {
         None
+    };
+    let reconstruct_path = |backtrack: HashMap<Node, Option<Node>>| -> Vec<Node> {
+        let mut current = end;
+        let mut out = Vec::new();
+        while let Some(next) = backtrack[&current] {
+            out.push(current);
+            current = next;
+        }
+        out.push(current); // Don't forget the start node
+        out.reverse();
+        out
     };
 
     while !boundary_nodes.is_empty() {
@@ -55,7 +63,7 @@ where
 
         // If the end is reached return the distance to the end and the path.
         if cur_node == end {
-            return Some((distances[&end], backtrack.map(|x| reconstruct_path(x))));
+            return Some((distances[&end], backtrack.map(reconstruct_path)));
         }
 
         // Increase scope of neighbors to neighbors of `cur_node`
@@ -68,9 +76,13 @@ where
             let proposed_distance = distances[&cur_node] + neighbor_edge_weight;
 
             // If don't already have a distance for the specified node or if the new distance is shorter
-            // replace the new distance for the neighbor
+            // replace the new distance for the neighbor and remember how this node was reached.
             if !distances.contains_key(&neighbor) || proposed_distance < distances[&neighbor] {
                 distances.insert(neighbor, proposed_distance);
+                backtrack = backtrack.map(|mut x| {
+                    x.insert(neighbor, Some(cur_node));
+                    x
+                });
                 boundary_nodes.insert(neighbor);
             }
         }
@@ -101,5 +113,42 @@ where
                 *i_j = (*i_j).min(i_k.saturating_add(&k_j));
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn astar_test() {
+        // let nodes = 1 -> 2 -> 3 -> 4
+        let neighbors = |x| {
+            if x < 4 {
+                vec![x + 1].into_iter()
+            } else {
+                vec![].into_iter()
+            }
+        };
+        assert_eq!(
+            astar(1, 4, neighbors, |x| 4 - x, |_, _| 1, true),
+            Some((3, Some(vec![1, 2, 3, 4])))
+        );
+
+        // let nodes = 1 -> 2 -> 3 -> 4 -> 1
+        //               -> 3
+        let neighbors = |x| {
+            if x == 1 {
+                vec![2, 3].into_iter()
+            } else if x < 4 {
+                vec![x + 1].into_iter()
+            } else {
+                vec![1].into_iter()
+            }
+        };
+        assert_eq!(
+            astar(1, 4, neighbors, |_| 0, |_, _| 1, true),
+            Some((2, Some(vec![1, 3, 4])))
+        )
     }
 }
