b7db14f7370b027442b2ed75daa5b0bc3be4d706
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-07-23 17:07:45 -0600

    solution

--- advent_20/src/main.rs
+++ advent_20/src/main.rs
@@ -71,9 +71,53 @@ mod part1 {
     }
 }
 
+mod part2 {
+    use super::*;
+    use crate::parse::parse_input;
+    use advent_lib::parse::read_and_leak;
+
+    const DECRYPT_KEY: isize = 811589153;
+
+    pub fn run(file_name: &str) -> Result<isize, Box<dyn Error>> {
+        let input = read_and_leak(file_name)?;
+        let cords = parse_input(input)?
+            .into_iter()
+            .map(|x| x * DECRYPT_KEY)
+            .collect::<Vec<_>>();
+
+        // Each has (id, value)
+        let mut identity_cords = cords.into_iter().enumerate().collect::<Vec<_>>();
+        let original = identity_cords.clone();
+        for _i in 0..10 {
+            for (i, cord) in original.clone() {
+                let pos = identity_cords
+                    .iter()
+                    .position(|x| x.0 == i)
+                    .expect("Same length");
+                let element = identity_cords.remove(pos);
+                identity_cords.insert(
+                    (pos + usize::try_from(cord.rem_euclid(
+                        isize::try_from(identity_cords.len()).expect("Too long for isize"),
+                    ))
+                    .expect("not negative"))
+                    .rem_euclid(identity_cords.len()),
+                    element,
+                );
+            }
+        }
+        let pos_0 = identity_cords
+            .iter()
+            .position(|x| x.1 == 0)
+            .expect("0 pivot exists");
+        Ok(identity_cords[(pos_0 + 1000) % identity_cords.len()].1
+            + identity_cords[(pos_0 + 2000) % identity_cords.len()].1
+            + identity_cords[(pos_0 + 3000) % identity_cords.len()].1)
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Part 1 answer: {:#?}", part1::run("input.txt")?);
-    // println!("Part 2 answer: {:#?}", part2::run("input.txt")?);
+    println!("Part 2 answer: {:#?}", part2::run("input.txt")?);
     Ok(())
 }
 
@@ -93,15 +137,15 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_part2() -> Result<(), Box<dyn Error>> {
-    //     assert_eq!(part2::run("inputtest.txt")?, 56 * 62);
-    //     Ok(())
-    // }
+    #[test]
+    fn test_part2() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2::run("inputtest.txt")?, 1623178306);
+        Ok(())
+    }
 
-    // #[test]
-    // fn part2_ans() -> Result<(), Box<dyn Error>> {
-    //     assert_eq!(part2::run("input.txt")?, 8250);
-    //     Ok(())
-    // }
+    #[test]
+    fn part2_ans() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2::run("input.txt")?, 4248669215955);
+        Ok(())
+    }
 }
