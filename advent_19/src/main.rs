02b16e9be363670eea3899b0ee7ddb5c6b8341cd
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-07-23 04:49:37 -0600

    Part2 solved. Slow and high memory usage, but less than 10 min Part 2 calculates ~91917846 states is probably why

--- advent_19/src/main.rs
+++ advent_19/src/main.rs
@@ -53,9 +53,55 @@ mod part1 {
     }
 }
 
+mod part2 {
+    use super::*;
+    use crate::{
+        data::{max_geodes, Robot, Round},
+        parse::parse_input,
+    };
+    use std::{cell::Cell, collections::BTreeMap, rc::Rc};
+
+    pub fn run(file_name: &str) -> Result<usize, Box<dyn Error>> {
+        let input = read_and_leak(file_name)?;
+        let blueprints = parse_input(input)?;
+        let starting_rounds = blueprints.into_iter().take(3).map(|blueprint| {
+            [
+                Round {
+                    blueprint: blueprint.clone(),
+                    robots: BTreeMap::from([(Robot::Ore, 1)]),
+                    target: Robot::Ore,
+                    ..Default::default()
+                },
+                Round {
+                    blueprint,
+                    robots: BTreeMap::from([(Robot::Ore, 1)]),
+                    target: Robot::Clay,
+                    ..Default::default()
+                },
+            ]
+        });
+        Ok({
+            let ret =
+                starting_rounds
+                    .into_iter()
+                    .map(|round| {
+                        let minutes = 32;
+                        max_geodes(round[0].clone(), minutes, Rc::new(Cell::new(0)))
+                            .max(max_geodes(round[1].clone(), minutes, Rc::new(Cell::new(0))))
+                    })
+                    .map(|quality_level| dbg!(usize::from(quality_level.unwrap())))
+                    .reduce(|acc, x| acc * x)
+                    .expect("Non empty");
+
+            unsafe { dbg!(data::CNT) };
+            ret
+        })
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Part 1 answer: {:#?}", part1::run("input.txt")?);
-    // println!("Part 2 answer: {:#?}", part2::run("input.txt")?);
+    println!("Part 2 answer: {:#?}", part2::run("input.txt")?);
     Ok(())
 }
 
@@ -75,15 +121,15 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_part2() -> Result<(), Box<dyn Error>> {
-    //     assert_eq!(part2::run("inputtest.txt")?, 58);
-    //     Ok(())
-    // }
+    #[test]
+    fn test_part2() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2::run("inputtest.txt")?, 56 * 62);
+        Ok(())
+    }
 
-    // #[test]
-    // fn part2_ans() -> Result<(), Box<dyn Error>> {
-    //     assert_eq!(part2::run("input.txt")?, 2080);
-    //     Ok(())
-    // }
+    #[test]
+    fn part2_ans() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2::run("input.txt")?, 8250);
+        Ok(())
+    }
 }
