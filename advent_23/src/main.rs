76f249dc86d37bc81b05c8b2a51312dec8c326a1
Easyoakland <97992568+Easyoakland@users.noreply.github.com>
2023-07-31 22:50:36 -0600

    parse with a global next dir

--- advent_23/src/main.rs
+++ advent_23/src/main.rs
@@ -2,8 +2,7 @@ use std::error::Error;
 
 mod data {
     use advent_lib::cord::NDCord;
-    use enum_iterator::{All, Sequence};
-    use std::iter::Cycle;
+    use enum_iterator::Sequence;
 
     pub type Val = isize;
     pub type Pos = NDCord<Val, 2>;
@@ -15,56 +14,21 @@ mod data {
         East,
         West,
     }
-
-    #[derive(Clone, Debug)]
-    pub struct Elf {
-        next_dir: Cycle<All<Dir>>,
-    }
-
-    impl Elf {
-        pub fn new() -> Self {
-            Elf {
-                next_dir: enum_iterator::all().cycle(),
-            }
-        }
-    }
-
-    impl Iterator for Elf {
-        type Item = Dir;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            self.next_dir.next()
-        }
-
-        fn size_hint(&self) -> (usize, Option<usize>) {
-            (usize::MAX, None)
-        }
-    }
-
-    impl Default for Elf {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
 }
 
 mod parse {
-    use crate::data::{Elf, Pos};
+    use crate::data::Pos;
     use advent_lib::parse::yap::{all_consuming, line_ending, ParseError};
-    use std::collections::BTreeMap;
+    use std::collections::BTreeSet;
     use yap::{IntoTokens, Tokens};
 
-    pub fn elf(input: &mut impl Tokens<Item = char>) -> Option<Elf> {
-        input.token('#').then(|| Elf::new())
-    }
-
-    pub fn initial_map(input: &mut impl Tokens<Item = char>) -> BTreeMap<Pos, Elf> {
-        let mut out = BTreeMap::new();
+    pub fn initial_map(input: &mut impl Tokens<Item = char>) -> BTreeSet<Pos> {
+        let mut out = BTreeSet::new();
         let mut cursor = Pos::default();
         loop {
             loop {
-                if let Some(elf) = elf(input) {
-                    out.insert(cursor, elf);
+                if input.token('#') {
+                    out.insert(cursor);
                     cursor[0] += 1;
                 } else if input.token('.') {
                     cursor[0] += 1;
@@ -81,19 +45,23 @@ mod parse {
         out
     }
 
-    pub fn parse_input(input: &str) -> Result<BTreeMap<Pos, Elf>, ParseError<char>> {
+    pub fn parse_input(input: &str) -> Result<BTreeSet<Pos>, ParseError<char>> {
         all_consuming(&mut input.into_tokens(), initial_map)
     }
 }
 
 mod part1 {
     use super::*;
-    use crate::{data::Val, parse::parse_input};
+    use crate::{
+        data::{Dir, Val},
+        parse::parse_input,
+    };
     use advent_lib::parse::read_and_leak;
 
     pub fn run(file_name: &str) -> Result<Val, Box<dyn Error>> {
         let input = read_and_leak(file_name)?;
         let map = parse_input(input)?;
+        let next_dir = enum_iterator::all::<Dir>().cycle();
         todo!()
     }
 }
